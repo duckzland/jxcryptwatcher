79f2abaca869d4d0a527b2fb0e1b0e1d445180b4
jason.xie@victheme.com <jason.xie@victheme.com>
2025-07-31 17:21:26 +0800

    refactor panel layout

--- panels/display.go
+++ panels/display.go
@@ -14,6 +14,71 @@ import (
 	JW "jxwatcher/widgets"
 )
 
+type PanelLayout struct{}
+
+func (p *PanelLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
+	if len(objects) < 5 {
+		return
+	}
+
+	bg := objects[0]
+	title := objects[1]
+	content := objects[2]
+	subtitle := objects[3]
+	action := objects[4]
+
+	// Full-size background
+	bg.Resize(size)
+	bg.Move(fyne.NewPos(0, 0))
+
+	// Filter visible content elements
+	centerItems := []fyne.CanvasObject{}
+	for _, obj := range []fyne.CanvasObject{title, content, subtitle} {
+		if obj.Visible() && obj.MinSize().Height > 0 {
+			centerItems = append(centerItems, obj)
+		}
+	}
+
+	// Compute total height of visible center items
+	var totalHeight float32
+	for _, obj := range centerItems {
+		totalHeight += obj.MinSize().Height
+	}
+
+	startY := (size.Height - totalHeight) / 2
+	currentY := startY
+
+	// Center stack
+	for _, obj := range centerItems {
+		objSize := obj.MinSize()
+		obj.Move(fyne.NewPos((size.Width-objSize.Width)/2, currentY))
+		obj.Resize(objSize)
+		currentY += objSize.Height
+	}
+
+	// Position action element last (highest z-index)
+	actionSize := action.MinSize()
+	action.Move(fyne.NewPos(size.Width-actionSize.Width, 0))
+	action.Resize(actionSize)
+}
+
+func (p *PanelLayout) MinSize(objects []fyne.CanvasObject) fyne.Size {
+	width := float32(0)
+	height := float32(0)
+
+	for _, obj := range objects[1:4] {
+		if obj.Visible() && obj.MinSize().Height > 0 {
+			size := obj.MinSize()
+			if size.Width > width {
+				width = size.Width
+			}
+			height += size.Height
+		}
+	}
+
+	return fyne.NewSize(width, height)
+}
+
 func NewPanelDisplay(
 	pdt *JT.PanelDataType,
 	onEdit func(pk string, uuid string),
