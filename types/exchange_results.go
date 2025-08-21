7f741fe6fe63af0efc257e3659dd62783832b184
jason.xie@victheme.com <jason.xie@victheme.com>
2025-08-21 13:09:10 +0800

    Added validation to prevent crash when invalid json received

--- types/exchange_results.go
+++ types/exchange_results.go
@@ -23,7 +23,7 @@ func (er *ExchangeResults) UnmarshalJSON(data []byte) error {
 		return err
 	}
 
-	if v["data"] == nil {
+	if er.validateData(v) == false {
 		return nil
 	}
 
@@ -31,6 +31,10 @@ func (er *ExchangeResults) UnmarshalJSON(data []byte) error {
 	tc := v["data"].(map[string]any)["quote"].([]any)
 
 	for _, rate := range tc {
+
+		if er.validateRate(rate) == false {
+			continue
+		}
 
 		ex := ExchangeDataType{}
 
@@ -47,6 +51,100 @@ func (er *ExchangeResults) UnmarshalJSON(data []byte) error {
 	}
 
 	return nil
+}
+
+func (er *ExchangeResults) validateData(v map[string]any) bool {
+
+	if _, ok := v["data"]; !ok {
+		JC.Logln("Missing 'data' field in exchange results")
+		return false
+	}
+
+	if _, ok := v["data"].(map[string]any); !ok {
+		JC.Logln("Invalid 'data' field format in exchange results")
+		return false
+	}
+
+	if _, ok := v["data"].(map[string]any)["symbol"]; !ok {
+		JC.Logln("Missing 'symbol' field in 'data'")
+		return false
+	}
+
+	if _, ok := v["data"].(map[string]any)["symbol"].(string); !ok {
+		JC.Logln("Invalid 'symbol' field type in 'data'")
+		return false
+	}
+
+	if _, ok := v["data"].(map[string]any)["id"]; !ok {
+		JC.Logln("Missing 'id' field in 'data'")
+		return false
+	}
+
+	if _, ok := v["data"].(map[string]any)["id"].(string); !ok {
+		JC.Logln("Invalid 'id' field type in 'data'")
+		return false
+	}
+
+	if _, ok := v["data"].(map[string]any)["amount"]; !ok {
+		JC.Logln("Missing 'amount' field in 'data'")
+		return false
+	}
+
+	if _, ok := v["data"].(map[string]any)["amount"].(float64); !ok {
+		JC.Logln("Invalid 'amount' field type in 'data'")
+		return false
+	}
+
+	if _, ok := v["data"].(map[string]any)["quote"]; !ok {
+		JC.Logln("Missing 'quote' field in 'data'")
+		return false
+	}
+
+	if _, ok := v["data"].(map[string]any)["quote"].([]any); !ok {
+		JC.Logln("Invalid 'quote' field type in 'data'")
+		return false
+	}
+
+	return true
+}
+
+func (er *ExchangeResults) validateRate(rate any) bool {
+	if _, ok := rate.(map[string]any); !ok {
+		JC.Logln("Invalid rate format:", rate)
+		return false
+	}
+
+	if _, ok := rate.(map[string]any)["symbol"]; !ok {
+		JC.Logln("Missing symbol in rate:", rate)
+		return false
+	}
+
+	if _, ok := rate.(map[string]any)["symbol"].(string); !ok {
+		JC.Logln("Invalid symbol type in rate:", rate)
+		return false
+	}
+
+	if _, ok := rate.(map[string]any)["cryptoId"]; !ok {
+		JC.Logln("Missing cryptoId in rate:", rate)
+		return false
+	}
+
+	if _, ok := rate.(map[string]any)["cryptoId"].(float64); !ok {
+		JC.Logln("Invalid cryptoId type in rate:", rate)
+		return false
+	}
+
+	if _, ok := rate.(map[string]any)["price"]; !ok {
+		JC.Logln("Missing price in rate:", rate)
+		return false
+	}
+
+	if _, ok := rate.(map[string]any)["price"].(float64); !ok {
+		JC.Logln("Invalid price type in rate:", rate)
+		return false
+	}
+
+	return true
 }
 
 func (er *ExchangeResults) GetRate(rk string) *ExchangeResults {
@@ -108,8 +206,25 @@ func (er *ExchangeResults) GetRate(rk string) *ExchangeResults {
 
 	defer resp.Body.Close()
 
+	c := resp.Body
+
+	// Debug
+	// payload := ""
+	// payload = "{}"
+	// payload = `{"data":[]}`
+	// payload = `{"data":{}}`
+	// payload = `{"data":{"quote":["SOL"]}}`
+	// payload = `{"data":{"id": "6636", "symbol": "DOT", "amount": 0.5, "quote":["SOL"]}}`
+	// payload = `{"data":{"id": "6636", "symbol": "DOT", "amount": 0.5, "quote":[{"SOL"}]}}`
+	// payload = `{"data":{"id": "6636", "symbol": "DOT", "amount": 0.5, "quote":[{"SOL":[]}]}}`
+	// payload = `{"data":{"id": "6636", "symbol": "DOT", "amount": 0.5, "quote":[{"SOL":{}}]}}`
+	// payload = `{"data":{"id": "6636", "symbol": "DOT", "amount": 0.5, "quote":[{"SOL":{"price":""}}]}}`
+	// payload = `{"data":{"id": "6636", "symbol": "DOT", "amount": 0.5, "quote":[{"SOL":{"price":"1", "symbol": "x"}}]}}`
+
+	// c = io.NopCloser(strings.NewReader(payload))
+
 	// Decode JSON directly from response body to save memory
-	decoder := json.NewDecoder(resp.Body)
+	decoder := json.NewDecoder(c)
 	if err := decoder.Decode(er); err != nil {
 		JC.Logln(fmt.Errorf("Failed to examine exchange data: %w", err))
 		return nil
