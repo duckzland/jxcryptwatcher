4867a6ce038bcd104eef279c50fe8f768eea47e5
jason.xie@victheme.com <jason.xie@victheme.com>
2025-09-15 00:06:12 +0800

    make actions manager to use private var and protect it with mutex

--- apps/actions.go
+++ apps/actions.go
@@ -1,32 +1,41 @@
 package apps
 
 import (
-	"fyne.io/fyne/v2"
+	"sync"
 
 	JW "jxwatcher/widgets"
+
+	"fyne.io/fyne/v2"
 )
 
 var AppActionManager *AppActions = &AppActions{}
 
 type AppActions struct {
-	Buttons []*JW.HoverCursorIconButton
+	mu      sync.RWMutex
+	buttons []*JW.HoverCursorIconButton
 }
 
 func (a *AppActions) Init() {
-	a.Buttons = []*JW.HoverCursorIconButton{}
+	a.mu.Lock()
+	a.buttons = []*JW.HoverCursorIconButton{}
+	a.mu.Unlock()
 }
 
 func (a *AppActions) AddButton(btn *JW.HoverCursorIconButton) {
-	a.Buttons = append(a.Buttons, btn)
+	a.mu.Lock()
+	a.buttons = append(a.buttons, btn)
+	a.mu.Unlock()
 }
 
 func (a *AppActions) GetButton(tag string) *JW.HoverCursorIconButton {
-	for _, btn := range a.Buttons {
+	a.mu.RLock()
+	defer a.mu.RUnlock()
+
+	for _, btn := range a.buttons {
 		if btn.GetTag() == tag {
 			return btn
 		}
 	}
-
 	return nil
 }
 
@@ -35,23 +44,27 @@ func (a *AppActions) CallButton(tag string) bool {
 	if btn == nil {
 		return false
 	}
-
 	btn.Call()
-
 	return true
 }
 
 func (a *AppActions) Refresh() {
+	a.mu.RLock()
+	defer a.mu.RUnlock()
+
 	fyne.Do(func() {
-		for _, btn := range a.Buttons {
+		for _, btn := range a.buttons {
 			btn.Refresh()
 		}
 	})
 }
 
 func (a *AppActions) Disable() {
+	a.mu.RLock()
+	defer a.mu.RUnlock()
+
 	fyne.Do(func() {
-		for _, btn := range a.Buttons {
+		for _, btn := range a.buttons {
 			btn.Disable()
 		}
 	})
