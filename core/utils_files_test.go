ac3c3c0dbcd1047ddfd505d3a26be5f4ccceaa57
jason.xie@victheme.com <jason.xie@victheme.com>
2025-09-30 00:28:56 +0800

    Adding log supressor for core files test

--- core/utils_files_test.go
+++ core/utils_files_test.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"encoding/json"
+	"log"
 	"os"
 	"strings"
 	"testing"
@@ -13,7 +14,28 @@ func init() {
 	_ = app.New() // Initializes a dummy Fyne app for storage access
 }
 
+type filesMockResponse struct {
+	Message string `json:"message"`
+}
+
+type filesNullWriter struct{}
+
+func (n filesNullWriter) Write(p []byte) (int, error) {
+	return len(p), nil
+}
+
+func filesTurnOffLogs() {
+	log.SetOutput(filesNullWriter{})
+}
+
+func filesTurnOnLogs() {
+	log.SetOutput(os.Stdout)
+}
+
 func TestGetUserDirectory(t *testing.T) {
+	filesTurnOffLogs()
+	defer filesTurnOnLogs()
+
 	dir := GetUserDirectory()
 	if dir == "" {
 		t.Error("Expected non-empty user directory path")
@@ -24,6 +46,9 @@ func TestGetUserDirectory(t *testing.T) {
 }
 
 func TestBuildPathRelatedToUserDirectory(t *testing.T) {
+	filesTurnOffLogs()
+	defer filesTurnOnLogs()
+
 	path := BuildPathRelatedToUserDirectory([]string{"testfile.json"})
 	if !strings.Contains(path, "testfile.json") {
 		t.Error("Expected path to include filename")
@@ -34,6 +59,9 @@ func TestBuildPathRelatedToUserDirectory(t *testing.T) {
 }
 
 func TestSaveAndLoadFile(t *testing.T) {
+	filesTurnOffLogs()
+	defer filesTurnOnLogs()
+
 	filename := "test_save_load.json"
 	testData := map[string]string{"hello": "world"}
 
@@ -59,6 +87,9 @@ func TestSaveAndLoadFile(t *testing.T) {
 }
 
 func TestCreateFileAndFileExists(t *testing.T) {
+	filesTurnOffLogs()
+	defer filesTurnOnLogs()
+
 	filename := "test_create_exists.txt"
 	fullPath := BuildPathRelatedToUserDirectory([]string{filename})
 
@@ -77,6 +108,9 @@ func TestCreateFileAndFileExists(t *testing.T) {
 }
 
 func TestCleanupCreatedFiles(t *testing.T) {
+	filesTurnOffLogs()
+	defer filesTurnOnLogs()
+
 	files := []string{"test_save_load.json", "test_create_exists.txt"}
 	for _, f := range files {
 		path := BuildPathRelatedToUserDirectory([]string{f})
